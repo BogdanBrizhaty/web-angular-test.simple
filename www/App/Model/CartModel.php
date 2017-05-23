0a77d900d243d5ae144e21ce006b001c6afb138f
BogdanBrizhaty <demaskinas@yandex.ua>
2017-05-23 23:18:05 +0300

    cart model

---
+++ www/App/Model/CartModel.php
@@ -0,0 +1,40 @@
+<?php
+    class CartModel extends BaseModel
+    {
+        public function __construct()
+        {
+            parent::__construct(new PhonesDataProvider());
+        }
+        public function procceedOrder($orderList)
+        {
+            // make sure orderList is not empty
+            if (!isset($orderList))
+                return (object)['status' => false, 'msg' => 'Order List is Empty', 'invalid_products' => null];
+
+            // if api is used not from our angular app 
+            // make sure all items are stored in our product list
+            $notFoundList = [];
+            foreach($orderList as $orderItem)
+            {
+                $isInList = false;
+                foreach($phones as $phone)
+                    if ($orderItem->Model == $phone->Model && $orderItem->Brand == $phone->Brand)
+                    {
+                        $isInList = true;
+                        break;
+                    }
+                if (!$isInList)
+                    array_push($notFoundList, $orderItem);
+            }
+
+            if (is_null($notFoundList) || count($notFoundList) == 0)
+            {
+                // send email to client
+                // send email to admin@shop.dev
+                return (object)['status' => true, 'msg' => ''];
+            }
+
+            return (object)['status' => false, 'msg' => 'Products were not found', 'invalid_products' => $notFoundList];
+        }
+    }
+?>
