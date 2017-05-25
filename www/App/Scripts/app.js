08c449b3052ad5ecbdb92c2ea3da6b1e18397096
BogdanBrizhaty <demaskinas@yandex.ua>
2017-05-25 18:31:45 +0300

    order processing basis

--- www/App/Scripts/app.js
+++ www/App/Scripts/app.js
@@ -12,13 +12,15 @@ angular.module('app', ['ngRoute'])
     })
 }])
 
-.service('productService', function () {
+.service('cartService', function () {
     var productList = [];
 
   var addProduct = (newObj) => {
       productList.push(newObj);
   };
-
+  var count = () => {
+      return productList.length;
+  }
   var getProducts = () =>{
       return productList;
   };
@@ -27,35 +29,95 @@ angular.module('app', ['ngRoute'])
         return e.Model == model && e.Brand == brand;
     })
   };
+  var removeProduct = (phone) => {
+    var index = productList.findIndex((e) => {
+        return e.Model == phone.Model && e.Brand == phone.Brand;
+    });
+    if (index !== -1)
+        productList.splice(index, 1);
+  };
 
   return {
     addProduct: addProduct,
     getProducts: getProducts,
-    getProduct: getProduct
+    count: count,
+    // getProduct: getProduct,
+    removeProduct: removeProduct
   };
 })
 
-.controller('AppController', ['$scope', '$http', 'productService', ($scope, $http, productService) => {
+.controller('AppController', ['$scope', '$http', 'cartService', '$location', ($scope, $http, cartService, $location) => {
     $http.get('/phonesapi/get?page=0').then((e) => {
         $scope.phones = e.data;
         $scope.phones.forEach((element) => {
             var url = element.Model;
             element.url = '/uploads/' + url.replace(/\s+/g, "").toLowerCase() + ".jpg";
+            element.InCart = false;
         })
     });
-    $scope.onAddButtonClick = function (phone) {
-        console.log('clicked ' + phone.Model);
-        productService.addProduct(phone);
+    $scope.itemsInCart = cartService.count();
+    $scope.productClickHandler = function(phone) {
+        if (phone.InCart)
+            cartService.removeProduct(phone);
+        else cartService.addProduct(phone);
+        
+        $scope.itemsInCart = cartService.count();
+        phone.InCart = phone.InCart ^ true;
+        console.log('handler');
+    };
+
+    $scope.goToCartHandler = () => {
+        $location.path('/cart');
     }
+
+    $scope.inCartToText = function (phone) {
+        return (phone.InCart) ? "Remove from cart" : "AddToCart";
+    };
 }])
 
-.controller('CartController', ['$scope', '$http', 'productService', ($scope, $http, productService) => {
+.controller('CartController', ['$scope', '$http', 'cartService', ($scope, $http, cartService) => {
     
-    $scope.orderlist = productService.getProducts();
+    $scope.orderlist = cartService.getProducts();
         var sum = 0.0;
         $scope.orderlist.forEach((e) => {
             e.Amount = 1;
             sum += e.Price;
-        })
+        });
         $scope.totalsum = sum;
+        console.log('cart');
+
+    $scope.procceedOrder = () => {
+        console.log('orderprocceed');
+        console.log(JSON.stringify(cartService.getProducts()));
+        $.ajax({
+            type: "POST",
+            url: '/cartapi/procceedorder',
+            data: 'order=' + JSON.stringify(cartService.getProducts()),
+            success: (response) => {
+                console.log(response);
+            },
+            error: (error) => {
+                console.log(error);
+            }
+            // dataType: dataType
+        });
+        // $http(
+        //     {
+        //         method: 'POST',
+        //         url: '/cartapi/procceedorder',
+        //         headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+        //         data: 'order=' + encodeURIComponent(JSON.stringify(cartService.getProducts()))
+        //         //'order=' + JSON.stringify(cartService.getProducts()),
+        //     }
+        // ).then((data, status, header, config) => {
+        //     console.log(data);
+        // },
+        // (error) => {
+        //     console.log(error);
+        // }
+        //  );
+
+        //  $()
+
+    }
 }]);
