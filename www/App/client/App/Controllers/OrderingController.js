3723604698a4d2eec9121d6efd7d52582b017d74
BogdanBrizhaty <demaskinas@yandex.ua>
2017-05-30 19:00:45 +0300

    ordering validation full

--- www/App/client/App/Controllers/OrderingController.js
+++ www/App/client/App/Controllers/OrderingController.js
@@ -7,44 +7,48 @@ app.controller('OrderingController', ['$scope', '$http', 'cartService', 'errorSe
         address: '',
         donotcallme: false
     };
-    
-    var nameRegEx = /^[A-Za-z\s]{0,30}$/;
-    var phoneRegEx = /^\+?[0-9]{0,12}$/;
-    
-    var emailLocalPartRegEx = /^[A-Za-z0-9!#$%&'*+\-\/=?^_`{|}~\.]{0,64}$/;
-    var emailDomainPartRegEx = /^([A-Za-z0-9\-]{1,63}\.){1,3}[A-Za-z0-9\-]{1,63}$/;
+    console.log($scope.userInfo);
 
-    var  getChar = function (event) {
-    if (event.which == null) { // IE
-        if (event.keyCode < 32) return null; // спец. символ
-        return String.fromCharCode(event.keyCode)
-    }
+    var  getChar = function (event) 
+    {
+        if (event.which == null) { // IE
+            if (event.keyCode < 32) return null; // спец. символ
+            return String.fromCharCode(event.keyCode)
+        }
 
-    if (event.which != 0 && event.charCode != 0) { // все кроме IE
-        if (event.which < 32) return null; // спец. символ
-        return String.fromCharCode(event.which); // остальные
-    }
+        if (event.which != 0 && event.charCode != 0) { // все кроме IE
+            if (event.which < 32) return null; // спец. символ
+            return String.fromCharCode(event.which); // остальные
+        }
 
-    return null; // спец. символ
+        return null; // спец. символ
     }
+    // REGION :: EVENT HANDLERS
+
+    // ON-TYPE
 
     $scope.nameKeyPressed = () => {
-        // regExValidation(nameRegEx, event);
-        // console.log(event.currentTarget.value);
+
+        var nameRegEx = /^[A-Za-z\s]{0,30}$/;
         var allowedSymbols = /[A-Za-z\s]/;
+
+        $scope.nameValidationError = '';
 
         var char = getChar(event);
         console.log (char);
         if (char === null)
             return;
 
-        event.preventDefault();
+        if (!allowedSymbols.test(char) || !nameRegEx.test(event.currentTarget.value + char))
+            event.preventDefault();
 
-        event.currentTarget.value += (allowedSymbols.test(char) && nameRegEx.test(event.currentTarget.value + char)) ? char : '';
+        // event.currentTarget.value += (allowedSymbols.test(char) && nameRegEx.test(event.currentTarget.value + char)) ? char : '';
 
     };
     $scope.emailKeyPressed = () => {
 
+        var emailLocalPartRegEx = /^[A-Za-z0-9!#$%&'*+\-\/=?^_`{|}~\.]{0,64}$/;
+        var emailDomainPartRegEx = /^([A-Za-z0-9\-]{1,63}\.){1,3}[A-Za-z0-9\-]{1,63}$/;
         var allowedLocalPartSymbols = /[A-Za-z0-9!#$%&'*+\-\/=?^_`{|}~\.]/;
         var allowedDomainPartSymbols = /[A-Za-z0-9\.\-]/;
         var partialDomainRegEx = /^[A-Za-z0-9\-]+\.?/;
@@ -55,6 +59,7 @@ app.controller('OrderingController', ['$scope', '$http', 'cartService', 'errorSe
 
         event.preventDefault();
 
+        // console.log(char);
 
         if (sender.value.includes('@'))
         {
@@ -65,11 +70,16 @@ app.controller('OrderingController', ['$scope', '$http', 'cartService', 'errorSe
 
             if (domain === '' && allowedDomainPartSymbols.test(char) && char !== '.' || partialDomainRegEx.test(domain + char) && !domain.includes('.'))
             {
+                $scope.userInfo.email += char;
+                // console.log(char);
                 sender.value += char;
                 return;
             }
             if (emailDomainPartRegEx.test(domain + char + 'a') || emailDomainPartRegEx.test(domain + char))
+            {
                 sender.value += char;
+                $scope.userInfo.email += char;
+            }                
         }
         else
         {
@@ -78,7 +88,11 @@ app.controller('OrderingController', ['$scope', '$http', 'cartService', 'errorSe
                 $scope.emailValidationError = 'email can not start with dot';
                 return;
             }
-            
+            if (sender.value.length == 0 && char === '@')
+            {
+                $scope.emailValidationError = 'email can not start with @';
+                return;
+            }
             if ((sender.value + char).includes('..'))
             {
                 $scope.emailValidationError = 'email can not contain two located together dots';
@@ -92,15 +106,25 @@ app.controller('OrderingController', ['$scope', '$http', 'cartService', 'errorSe
                     $scope.emailValidationError = 'Left part of email address can not end with dot!'
                     return;
                 }
+                $scope.userInfo.email += char;
                 sender.value += char;
             }
-            sender.value += (allowedLocalPartSymbols.test(char) && emailLocalPartRegEx.test(sender.value + char)) ? char : '';
+            if (allowedLocalPartSymbols.test(char) && emailLocalPartRegEx.test(sender.value + char))
+            {
+                sender.value += char;
+                $scope.userInfo.email += char;                
+            }
+            // sender.value += (allowedLocalPartSymbols.test(char) && emailLocalPartRegEx.test(sender.value + char)) ? char : '';
         }
         
     };
     $scope.phoneKeyPressed = () => {
 
+        var phoneRegEx = /^\+?[0-9]{0,12}$/;
         var allowedSymbols = /[0-9]/;
+
+        $scope.phoneValidationError = '';
+
 
         var char = getChar(event);
         var sender = event.currentTarget;
@@ -118,12 +142,120 @@ app.controller('OrderingController', ['$scope', '$http', 'cartService', 'errorSe
         sender.value += (sender.value.length == 12 + offset) ? ' ' : '';
         sender.value += (sender.value.length == 16 + offset) ? ' ' : '';
 
+        $scope.userInfo.phone = sender.value.replace(/[\(\)\s]/g, '');
+        console.log(sender.value.replace(/[\(\)\s]/g, ''));
+
     };
+    $scope.addressKeyPressed = () => {
+        $scope.addressValidationError = '';
+    };
+
+    // ON KEY UP
+    $scope.emailKeyUp = () => {
+        console.log(getChar(event));
+        if (getChar(event) == null) 
+            $scope.userInfo.email = event.currentTarget.value;
+    };
+    $scope.phoneKeyUp = () => {
+        console.log(getChar(event));
+        if (getChar(event) == null) 
+            $scope.userInfo.phone = event.currentTarget.value.replace(/[\(\)\s]/g, '');
+    };
+
+    // ON FOCUSOUT(blur)
+
+    $scope.nameFocusOutEventHandler = () => {
+        $scope.nameValidationError = '';
+        console.log('name leave');
+        if ($scope.userInfo.name.length == 0)
+        {
+            $scope.nameValidationError = 'Name is empty';
+            return false;
+        }
+        if ($scope.userInfo.name.length < 4)
+        {
+            $scope.nameValidationError = 'Name is too short';
+            return false;
+        }
+        // TRUE if no Errors
+        return true;
+    };
+    $scope.emailFocusOutEventHandler = () => {
+        $scope.emailValidationError = '';
+        if ($scope.userInfo.email.length == 0)
+        {
+            $scope.emailValidationError = 'Email is empty';
+            return false;
+        }
+        var domain = $scope.userInfo.email.substring($scope.userInfo.email.indexOf('@') + 1, $scope.userInfo.email.length);
+        if (!$scope.userInfo.email.includes('@') || domain.length == 0 || !domain.includes('.') || domain.endsWith('.'))
+        {
+            $scope.emailValidationError = 'Sorry, this is not email';
+            return false;
+        }
+        if ($scope.userInfo.email.length < 5)
+        {
+            $scope.emailValidationError = 'Email is too short';
+            return false;
+        }
+
+        return true;
+    };
+    $scope.phoneFocusOutEventHandler = () => {
+        if ($scope.userInfo.donotcallme)
+            return true;
+        if ($scope.userInfo.phone.length == 0)
+        {
+            $scope.phoneValidationError = 'Phone is empty';
+            return false;
+        }
+        if ($scope.userInfo.phone.length < 12)
+        {
+            $scope.phoneValidationError = 'Phone is too short';
+            return false;
+        }
+
+        return true;
+    };
+    $scope.addressFocusOutEventHandler = () => {
+        $scope.addressValidationError = '';
+        if ($scope.userInfo.address.length == 0)
+        {
+            $scope.addressValidationError = 'Address is empty';
+            return false;
+        }
+        if ($scope.userInfo.address.length < 4)
+        {
+            $scope.addressValidationError = 'Address is too short';
+            return false;
+        }
+        // TRUE if no Errors
+        return true;
+    };
+
+    // ON CHECKBOX CHANGE STATE
+
+    $scope.donotcallmeChanged = () => {
+        if (event.currentTarget.checked)
+            $scope.phoneValidationError = '';
+        else 
+            $scope.phoneFocusOutEventHandler();
+    };
+
+    // DATA SUBMITTING
 
     $scope.submit = () => {
         console.log('orderprocceed');
         console.log(JSON.stringify(cartService.getProducts()));
 
+        if (!$scope.nameFocusOutEventHandler() || !$scope.emailFocusOutEventHandler() || !$scope.phoneFocusOutEventHandler() || !$scope.addressFocusOutEventHandler())
+        {
+            $scope.submitError = 'Please, complete the form first';
+            return;
+        }
+
+
+        // submitting data and redirecting
         $http(
             {
                 method: 'POST',
@@ -146,7 +278,7 @@ app.controller('OrderingController', ['$scope', '$http', 'cartService', 'errorSe
             (error) => {
                 console.log(error);
             }
-        )
+        );
     }
 
 }]);
